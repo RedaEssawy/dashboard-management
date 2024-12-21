b36750256c56d9b4af916215e0323173cbd30e3c
mohamed <mohamedelsisi211@gmail.com>
2024-12-22 00:37:11 +0200

    finishing first 2 stages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateProjectComponent } from './pages/create-project/create-project.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ResourceAllocationComponent } from './pages/resource-allocation/resource-allocation.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateProjectComponent,
+    ProjectsComponent,
+    ResourceAllocationComponent,
+    ReportsComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
